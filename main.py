558255e49906e00d2bb8183874ef955c18eb898b
Ksenia Bolshakova <ksenia.bolshakova@quantlane.com>
2023-05-18 16:26:41 +0200

    Unzip template

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+from robotics import Robot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+SCIENTISTS = ["Albert Einstein", "Isaac Newton", "Marie Curie", "Charles Darwin"]
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+robot = Robot("Quandrinaut")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def introduce_yourself():
+    robot.say_hello()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def main():
+    introduce_yourself()
+
+
+if __name__ == "__main__":
+    main()
